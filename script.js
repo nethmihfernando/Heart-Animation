66854346d8ba7bcae76dc8e7579d3be9606eb2d0
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-05-02 06:37:21 +0530

    Imports

---
+++ script.js
@@ -0,0 +1,6 @@
+import * as THREE from 'three';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
