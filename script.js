4814e2efd7ab2013c33cfa832aacce9bce2cfeea
nethmihimashi108@gmail.com <nethmihimashi108@gmail.com>
2026-05-02 06:43:21 +0530

    Variables

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+let scene, camera, renderer, particles, composer, controls;
+let time = 0;
+let isAnimationEnabled = true;
+let currentTheme = 'molten';
+let morphTarget = 0;
+let morphProgress = 0;
+
+const particleCount = 10000;
